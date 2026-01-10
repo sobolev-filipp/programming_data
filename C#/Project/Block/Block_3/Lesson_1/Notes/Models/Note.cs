b5684bd09b563b4af4ea9991f5e8ba392f9c628a
Filipp <filipp.sobolev1999@mail.ru>
2026-01-10 16:04:10 +0700

    Добавил проект Заметки для Блок 3 Урок 1

---
+++ C#/Project/Block/Block_3/Lesson_1/Notes/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Notes.Models
+{
+    internal class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
